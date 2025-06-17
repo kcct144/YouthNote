76ae1f8c7abb55ea32b705ab8e87aba43e6ed0f3
unknown <23312091@qq.com>
2025-06-17 16:23:27 +0800

    ing

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -1,18 +1,16 @@
-import { defineThemeConfig } from 'vuepress-theme-plume'
-import { navbar } from './navbar'
-import { notes } from './notes'
+import { defineThemeConfig } from "vuepress-theme-plume";
+import { navbar } from "./navbar";
+import { notes } from "./notes";
 
 /**
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: "https://theme-plume.vuejs.press/plume.png",
 
-  appearance: true,  // 配置 深色模式
+  appearance: true, // 配置 深色模式
 
-  social: [
-    { icon: 'github', link: '/' },
-  ],
+  // social: [{ icon: "github", link: "/" }],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
@@ -37,9 +35,9 @@ export default defineThemeConfig({
    * @see https://theme-plume.vuejs.press/config/basic/#profile
    */
   profile: {
-    avatar: 'https://theme-plume.vuejs.press/plume.png',
-    name: 'Senior Notes',
-    description: '初中知识笔记',
+    avatar: "https://theme-plume.vuejs.press/plume.png",
+    name: "Senior Notes",
+    description: "初中知识笔记",
     // circle: true,
     // location: '',
     // organization: '',
@@ -52,18 +50,17 @@ export default defineThemeConfig({
    * 公告板
    * @see https://theme-plume.vuejs.press/guide/features/bulletin/
    */
-  bulletin: {
-    layout: 'top-right',
-    contentType: 'markdown',
-    title: '公告板标题',
-    content: '公告板内容',
-  },
+  // bulletin: {
+  //   layout: 'top-right',
+  //   contentType: 'markdown',
+  //   title: '公告板标题',
+  //   content: '公告板内容',
+  // },
 
   /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
   transition: {
-    page: true,        // 启用 页面间跳转过渡动画
-    postList: true,    // 启用 博客文章列表过渡动画
-    appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
+    page: true, // 启用 页面间跳转过渡动画
+    postList: true, // 启用 博客文章列表过渡动画
+    appearance: "fade", // 启用 深色模式切换过渡动画, 或配置过渡动画类型
   },
-
-})
+});
